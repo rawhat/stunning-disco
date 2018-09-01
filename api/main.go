7397c1ce4d408ec51c787523ddfc7a22654fce5f
Alex Manning <alex41290@gmail.com>
2018-09-01 16:59:51 -0400

    added websockets... almost there. and go fmt

--- api/main.go
+++ api/main.go
@@ -1,24 +1,25 @@
 package main
 
 import (
-  "database/sql"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
 
-  "github.com/gorilla/handlers"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-  "github.com/streadway/amqp"
-  _ "github.com/lib/pq"
+	"github.com/gorilla/websocket"
+	_ "github.com/lib/pq"
+	"github.com/streadway/amqp"
 )
 
 type ChannelQueues struct {
-  CommandChannel *amqp.Channel
-  LogChannel     *amqp.Channel
+	CommandChannel *amqp.Channel
+	LogChannel     *amqp.Channel
 }
 
 type Database struct {
-  db *sql.DB
+	db *sql.DB
 }
 
 func SayHello(w http.ResponseWriter, r *http.Request) {
@@ -35,76 +36,96 @@ type Response struct {
 	Message string `json:"message"`
 }
 
+type WebsocketMessage struct {
+	Action  string `json:"action"`
+	Message interface{}
+}
+
+type WebsocketRegister struct {
+	Username string `json:"username"`
+}
+
 type SubmitRequest struct {
-  Language string `json:"language"`
-  Script   string `json:"script"`
-  Username string `json:"username"`
+	Language string `json:"language"`
+	Script   string `json:"script"`
+	Username string `json:"username"`
 }
 
 type ContainerLog struct {
-  Username string `json:"username"`
-  Log      string `json:"log"`
+	Username string `json:"username"`
+	Log      string `json:"log"`
 }
 
+var channelQueue *ChannelQueues
+var database *Database
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		// TODO:  fix me, obvi
+		return true
+	},
+}
+var wsUsers = make(map[string]*websocket.Conn)
+
 func InitQueues() (*amqp.Channel, *amqp.Channel) {
-  conn1, err := amqp.Dial("amqp://queue/")
-  if err != nil {
-    panic(err)
-  }
-  //defer conn.Close()
-  commandChannel, err := conn1.Channel()
-  if err != nil {
-    panic(err)
-  }
-  //defer ch.Close()
-  _, err = commandChannel.QueueDeclare(
-    "commands",
-    false,
-    false,
-    false,
-    false,
-    nil,
-  )
-  if err != nil {
-    panic(err)
-  }
-  conn2, err := amqp.Dial("amqp://queue/")
-  if err != nil {
-    panic(err)
-  }
-  //defer conn.Close()
-  logChannel, err := conn2.Channel()
-  if err != nil {
-    panic(err)
-  }
-  _, err = logChannel.QueueDeclare(
-    "logs",
-    false,
-    false,
-    false,
-    false,
-    nil,
-  )
-  if err != nil {
-    panic(err)
-  }
-  return commandChannel, logChannel
+	conn1, err := amqp.Dial("amqp://queue/")
+	if err != nil {
+		panic(err)
+	}
+	//defer conn.Close()
+	commandChannel, err := conn1.Channel()
+	if err != nil {
+		panic(err)
+	}
+	//defer ch.Close()
+	_, err = commandChannel.QueueDeclare(
+		"commands",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		panic(err)
+	}
+	conn2, err := amqp.Dial("amqp://queue/")
+	if err != nil {
+		panic(err)
+	}
+	//defer conn.Close()
+	logChannel, err := conn2.Channel()
+	if err != nil {
+		panic(err)
+	}
+	_, err = logChannel.QueueDeclare(
+		"logs",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		panic(err)
+	}
+	return commandChannel, logChannel
 }
 
 func InitDb() *Database {
-  connString := "host=db user=postgres dbname=postgres sslmode=disable"
-  db, err := sql.Open("postgres", connString)
-  if err != nil {
-    panic(err)
-  }
-  return &Database{db: db}
+	connString := "host=db user=postgres dbname=postgres sslmode=disable"
+	db, err := sql.Open("postgres", connString)
+	if err != nil {
+		panic(err)
+	}
+	return &Database{db: db}
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
 	login := &LoginRequest{}
 	err := json.NewDecoder(r.Body).Decode(&login)
 	if err != nil {
-		panic(err)
+		//panic(err)
+		return
 	}
 	response := &Response{}
 	if login.Username == "username" && login.Password == "password" {
@@ -118,89 +139,143 @@ func Login(w http.ResponseWriter, r *http.Request) {
 }
 
 func NewUser(w http.ResponseWriter, r *http.Request) {
-  // TODO: change name
-  create := &LoginRequest{}
-  err := json.NewDecoder(r.Body).Decode(&create)
-  if err != nil {
-    panic(err)
-  }
-  response := &Response{}
-  err = database.CreateUser(create.Username, create.Password)
-  if err != nil {
-    response = &Response{Status: 403, Message: "invalid"}
-  } else {
-    response = &Response{Status: 200, Message: "ok"}
-  }
-  json.NewEncoder(w).Encode(response)
+	// TODO: change name
+	create := &LoginRequest{}
+	err := json.NewDecoder(r.Body).Decode(&create)
+	if err != nil {
+		//panic(err)
+		return
+	}
+	response := &Response{}
+	err = database.CreateUser(create.Username, create.Password)
+	if err != nil {
+		response = &Response{Status: 403, Message: "invalid"}
+	} else {
+		response = &Response{Status: 200, Message: "ok"}
+	}
+	json.NewEncoder(w).Encode(response)
 }
 
 func Submit(w http.ResponseWriter, r *http.Request) {
-  submission := &SubmitRequest{}
-  err := json.NewDecoder(r.Body).Decode(&submission)
-  if err != nil {
-    panic(err)
-  }
-  body, err := json.Marshal(submission)
-  channelQueue.SendCommand(body)
-  response := &Response{Status: 200, Message: "ok"}
-  json.NewEncoder(w).Encode(response)
+	submission := &SubmitRequest{}
+	err := json.NewDecoder(r.Body).Decode(&submission)
+	if err != nil {
+		//panic(err)
+		return
+	}
+	body, err := json.Marshal(submission)
+	channelQueue.SendCommand(body)
+	response := &Response{Status: 200, Message: "ok"}
+	json.NewEncoder(w).Encode(response)
+}
+
+func Coder(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Printf("upgrade: %v", err)
+		return
+	}
+	defer SocketDisconnect(c)
+	for {
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			fmt.Printf("read: %v", err)
+			break
+		}
+		var msg json.RawMessage
+		envelope := &WebsocketMessage{
+			Message: &msg,
+		}
+		if err := json.Unmarshal(message, envelope); err != nil {
+			fmt.Printf("error decoding json: %v", err)
+			break
+		}
+		switch envelope.Action {
+		case "register":
+			var r WebsocketRegister
+			if err := json.Unmarshal(msg, &r); err != nil {
+				fmt.Printf("error registering: %v", err)
+				break
+			}
+			fmt.Printf("user registered: %v", r.Username)
+			wsUsers[r.Username] = c
+		default:
+			break
+		}
+	}
+}
+
+func SocketDisconnect(s *websocket.Conn) {
+	// TODO: is this scalable?  maybe pass in username or something
+	for username, conn := range wsUsers {
+		if conn == s {
+			delete(wsUsers, username)
+			break
+		}
+	}
 }
 
 func (queue *ChannelQueues) SendCommand(submission []byte) {
-  err := queue.CommandChannel.Publish(
-    "",
-    "commands",
-    false,
-    false,
-    amqp.Publishing {
-      ContentType: "application/json",
-      Body:        submission,
-    },
-  )
-  if err != nil {
-    panic(err)
-  }
+	err := queue.CommandChannel.Publish(
+		"",
+		"commands",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        submission,
+		},
+	)
+	if err != nil {
+		//panic(err)
+		return
+	}
 }
 
 func (queue *ChannelQueues) ListenForLogs() {
-  logs, err := queue.LogChannel.Consume("", "logs", false, false, false, false, nil)
-  if err != nil {
-    panic(err)
-  }
-  go func() {
-    for log := range logs {
-      response := &ContainerLog{}
-      json.Unmarshal(log.Body, &response)
-      fmt.Printf("got a log: %v", response)
-    }
-  }()
+	logs, err := queue.LogChannel.Consume("", "logs", false, false, false, false, nil)
+	if err != nil {
+		//panic(err)
+		return
+	}
+	go func() {
+		for log := range logs {
+			response := &ContainerLog{}
+			json.Unmarshal(log.Body, &response)
+			fmt.Printf("user map is: %v", wsUsers)
+			if _, ok := wsUsers[response.Username]; ok {
+				fmt.Printf("going to write to user: %v", response.Username)
+				wsUsers[response.Username].WriteMessage(websocket.TextMessage, []byte(response.Log))
+			} else {
+				fmt.Printf("user not found in map")
+			}
+		}
+	}()
 }
 
 func (db *Database) CreateUser(username string, password string) error {
-  _, err := db.db.Query("INSERT INTO users(username, password) VALUES ($1, $2)", username, password)
-  return err
+	_, err := db.db.Query("INSERT INTO users(username, password) VALUES ($1, $2)", username, password)
+	return err
 }
 
-var database *Database
-var channelQueue *ChannelQueues
-
 func main() {
-  database = InitDb()
-  commandChannel, logChannel := InitQueues()
-  channelQueue = &ChannelQueues{
-    CommandChannel: commandChannel,
-    LogChannel: logChannel,
-  }
-  channelQueue.ListenForLogs()
+	database = InitDb()
+	commandChannel, logChannel := InitQueues()
+	channelQueue = &ChannelQueues{
+		CommandChannel: commandChannel,
+		LogChannel:     logChannel,
+	}
+	channelQueue.ListenForLogs()
 	router := mux.NewRouter()
 	router.HandleFunc("/", SayHello)
 	router.HandleFunc("/login", Login)
-  router.HandleFunc("/user/create", NewUser)
-  router.HandleFunc("/submit", Submit)
+	router.HandleFunc("/user/create", NewUser)
+	router.HandleFunc("/submit", Submit)
+	router.HandleFunc("/coder", Coder)
 
-  allowedHeaders := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type"})
-  allowedOrigins := handlers.AllowedOrigins([]string{"*"})
-  allowedMethods := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"})
+	allowedHeaders := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type"})
+	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
+	allowedMethods := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"})
 
 	if err := http.ListenAndServe(":3000", handlers.CORS(allowedHeaders, allowedOrigins, allowedMethods)(router)); err != nil {
 		panic(err)
